dc3d63562c3dd576bba0824900d1c1d3d0970c40
SameerJadav <sameerjadav010@gmail.com>
2023-03-26 17:29:35 +0530

    Added auth with clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
@@ -5,7 +6,11 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
