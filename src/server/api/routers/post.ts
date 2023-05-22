eb435c4a5dab466c9ab72321780627bee22b52a0
SameerJadav <sameerjadav010@gmail.com>
2023-05-22 11:50:11 +0530

    Removed ratelimiting

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -2,8 +2,6 @@ import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-import { Ratelimit } from "@upstash/ratelimit";
-import { Redis } from "@upstash/redis";
 
 import {
   createTRPCRouter,
@@ -18,13 +16,6 @@ const filterUserForClient = (user: User) => {
     // lastName: user.lastName,
   };
 };
-
-// Create a new ratelimiter, that allows 5 requests per 1 minute
-const ratelimit = new Ratelimit({
-  redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(5, "1 m"),
-  analytics: true,
-});
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
@@ -64,12 +55,6 @@ export const postsRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId;
-
-      // User cannot spam posts
-      // They can't even get this far because they have to have a valid cookie
-      const { success } = await ratelimit.limit(authorId);
-
-      if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
 
       const post = await ctx.prisma.post.create({
         data: {
