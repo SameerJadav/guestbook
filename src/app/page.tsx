95ed0b60db269725579e81b2ed2e4e9211479db5
SameerJadav <sameerjadav010@gmail.com>
2023-07-04 14:49:52 +0530

    feat: Add navigation links to the bottom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,15 @@
-import { cn } from "~/lib/utils"
+import Heading from "~/components/heading"
+import Nav from "~/components/nav"
 import PostWizard from "~/components/post-wizard"
-import { noto } from "./layout"
 
 export default function Home() {
   return (
-    <div>
-      <h1
-        className={cn(
-          "text-center text-6xl md:mt-6 md:text-8xl",
-          noto.className
-        )}
-      >
-        Guestbook
-      </h1>
-
+    <>
+      <Heading />
       <div className="mt-6 border-b border-slate6 pb-6">
         <PostWizard />
       </div>
-    </div>
+      <Nav />
+    </>
   )
 }
