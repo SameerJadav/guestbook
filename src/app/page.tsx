c94e48c1e76a5c57d8e608e2ee656c4abff48f8b
SameerJadav <sameerjadav010@gmail.com>
2023-07-07 00:13:23 +0530

    Revert "feat: Implement loading skeleton"
    This reverts commit f2f2b652916a15587a43899fd50e7f5d9dff860f.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import { Suspense } from "react"
 import { fetchPosts } from "~/server/posts"
-import { Skeleton } from "~/components/ui/skeleton"
 import Heading from "~/components/Heading"
 import Nav from "~/components/Nav"
 import PostWizard from "~/components/PostWizard"
@@ -13,17 +11,12 @@ export default async function Home() {
       <PostWizard />
       <div className="mt-6 space-y-2">
         {data.map((data) => (
-          <Suspense
-            key={data.post.id}
-            fallback={<Skeleton className="w-full h-6" />}
-          >
-            <p>
-              <span className="text-slate11">
-                {data.author?.firstName} {data.author?.lastName}:
-              </span>{" "}
-              {data.post.content}
-            </p>
-          </Suspense>
+          <p key={data.post.id}>
+            <span className="text-slate11">
+              {data.author?.firstName} {data.author?.lastName}:
+            </span>{" "}
+            {data.post.content}
+          </p>
         ))}
       </div>
       <Nav />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,8 @@ import { type Config } from "tailwindcss"
 
 export default {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/ui/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
