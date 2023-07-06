ff4745304da7efd86c9a88b0a58cc3b4b74fa4f2
SameerJadav <sameerjadav010@gmail.com>
2023-07-06 18:54:35 +0530

    feat: Fetch posts data from the backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
+import { fetchPosts } from "~/server/posts"
 import Heading from "~/components/Heading"
 import Nav from "~/components/Nav"
 import PostWizard from "~/components/PostWizard"
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetchPosts()
   return (
     <>
       <Heading />
-      <div className="mt-6 border-b border-slate6 pb-6">
-        <PostWizard />
+      <PostWizard />
+      <div className="mt-6 space-y-2">
+        {data.map((data) => (
+          <p key={data.post.id}>
+            <span className="text-slate11">
+              {data.author?.firstName} {data.author?.lastName}:
+            </span>{" "}
+            {data.post.content}
+          </p>
+        ))}
       </div>
       <Nav />
     </>
