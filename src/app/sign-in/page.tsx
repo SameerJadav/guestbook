59a0123836bd2f9ed42ab3b9e0d898d7412a452e
SameerJadav <sameerjadav010@gmail.com>
2023-07-11 16:06:51 +0530

    added `site.webmanifest`

--- src/app/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs"
-
-export default function Page() {
-  return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <SignIn />
-    </div>
-  )
-}
