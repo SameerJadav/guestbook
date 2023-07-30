4546f64e09c2a01c67e51352449ba71a5a7ff83e
SameerJadav <sameerjadav010@gmail.com>
2023-07-30 09:56:45 +0530

    chore: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { type Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ClerkProvider } from "@clerk/nextjs"
+import { Analytics } from "@vercel/analytics/react"
 import { siteConfig } from "~/config"
 import { cn } from "~/lib/utils"
 import "~/styles/globals.css"
@@ -79,6 +80,7 @@ export default function RootLayout({
               {children}
             </main>
           </Providers>
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
