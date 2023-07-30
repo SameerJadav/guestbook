89401871d863e9c45e03852440fbaa584ca2ed96
SameerJadav <sameerjadav010@gmail.com>
2023-07-30 11:41:03 +0530

    refactor: redesign footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,25 @@
 import { siteConfig } from "~/config"
+import { Icons } from "~/components/Icons"
 import ExternalLink from "./ExternalLink"
 
 export default function Footer() {
   return (
-    <footer className="mt-4 flex items-center justify-center gap-2 border-t border-slate6 py-4">
-      <ExternalLink href={siteConfig.links.twitter}>Twitter</ExternalLink>∙
-      <ExternalLink href={siteConfig.links.github}>Github</ExternalLink>∙
-      <ExternalLink href={siteConfig.links.linkedin}>LinkedIn</ExternalLink>∙
-      <ExternalLink href={siteConfig.links.instagram}>Instagram</ExternalLink>∙
-      <ExternalLink href={siteConfig.links.mail}>Mail</ExternalLink>
+    <footer className="mt-4 flex w-full items-center justify-between border-t border-slate6 py-4">
+      <p>
+        Built by{" "}
+        <ExternalLink href={siteConfig.links.twitter}>
+          Sameer Jadav
+        </ExternalLink>
+        .
+      </p>
+      <div className="flex items-center gap-4">
+        <ExternalLink href={siteConfig.links.github}>
+          <Icons.github className="h-5 w-5" />
+        </ExternalLink>
+        <ExternalLink href={siteConfig.links.mail}>
+          <Icons.mail className="h-5 w-5" />
+        </ExternalLink>
+      </div>
     </footer>
   )
 }
