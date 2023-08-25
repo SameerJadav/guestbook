736d19c836a5b55a9cff1bf42bb644b11dd50a3c
SameerJadav <sameerjadav010@gmail.com>
2023-08-25 08:33:31 +0530

    refact: added descriptive names for link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import ExternalLink from "./ExternalLink"
 
 export default function Footer() {
   return (
-    <footer className="border-gray6 mt-4 flex w-full items-center justify-between border-t py-4">
+    <footer className="mt-4 flex w-full items-center justify-between border-t border-gray6 py-4">
       <p>
         Built by{" "}
         <ExternalLink href={SITE.links.twitter}>Sameer Jadav</ExternalLink>.
@@ -12,9 +12,11 @@ export default function Footer() {
       <div className="flex items-center gap-4">
         <ExternalLink href={SITE.links.github}>
           <Icons.github className="h-5 w-5" />
+          <p className="sr-only">Github</p>
         </ExternalLink>
         <ExternalLink href={SITE.links.mail}>
           <Icons.mail className="h-5 w-5" />
+          <p className="sr-only">Mail</p>
         </ExternalLink>
       </div>
     </footer>
