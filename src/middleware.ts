5e5e24696441b044e3b70f669fa5615d3151eca0
SameerJadav <sameerjadav010@gmail.com>
2023-11-01 11:35:53 +0530

    fix: resolve unauthorized error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,11 @@
+import { NextResponse } from "next/server";
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/"],
+  afterAuth(auth) {
+    if (auth.isPublicRoute) NextResponse.next();
+  },
 });
 
 export const config = {
