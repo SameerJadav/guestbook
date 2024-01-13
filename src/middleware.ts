9eda99dc479f3ea1a8f117538ac00ec6feeecf34
SameerJadav <sameerjadav010@gmail.com>
2024-01-13 13:41:08 +0530

    revert middleware changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,13 @@
+import { NextResponse } from "next/server";
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/"],
+  afterAuth(auth) {
+    if (auth.isPublicRoute) NextResponse.next();
+  },
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)"],
+  matcher: ["/((?!.*\\..*|_next).*)"],
 };
