d2949bf84044664bc690ec01a19817079aac9ce9
SameerJadav <sameerjadav010@gmail.com>
2024-01-29 14:13:04 +0530

    chore: add metadata

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,16 @@
 export const site = {
+  name: "Guestbook",
+  description:
+    "Beautiful and user-friendly guestbook app. Open source. Performant. Accessible.",
+  url:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://guestbook.sameerjadav.me",
   author: {
     name: "Sameer Jadav",
     web: "https://sameerjadav.me",
     github: "https://github.com/SameerJadav",
     twitter: "https://twitter.com/SameerJadav_",
+    twitterId: "@SameerJadav_",
   },
 };
